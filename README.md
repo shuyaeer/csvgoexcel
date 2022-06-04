28d430e4e7e8b997dd0ba603d0fd5c68ffea3586
Shuya Fukushima <shuya102711@gmail.com>
2022-06-05 01:50:44 +0900

    readme作成 出力されるexcelファイル名を入力ファイル名と対応付け

---
+++ README.md
@@ -0,0 +1,19 @@
+# CsvGoExcel
+
+CsvGoExcel is a simple comverting tool between csv and xlsx.
+
+# How To Use
+
+```bash
+$ go run main sample.csv
+sample.xlsx is generated
+```
+
+More options are now developing.
+
+- be cli
+- excel to csv
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/csv"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
@@ -14,14 +17,13 @@ import (
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	file, err := os.Open(args[0])
+	fileName := args[0]
+	file, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-
 	r := csv.NewReader(file)
-
 	line_index := 1
 	writer := excelize.NewFile()
 	for {
@@ -32,12 +34,17 @@ func main() {
 		create_elsx(row, line_index, writer)
 		line_index = line_index + 1
 	}
-	writer.SaveAs("./Book1.xlsx")
-
+	fileExt := filepath.Ext(fileName)
+	withoutExt := strings.Replace(fileName, fileExt, "", 1)
+	f := strings.Split(withoutExt, "/")
+	excelFileName := f[len(f)-1] + ".xlsx"
+	fmt.Println(excelFileName + " is generated")
+	writer.SaveAs("./" + excelFileName)
 }
 
 func create_elsx(row []string, line_index int, writer *excelize.File) {
-	m := map[int]string{0: "A", 1: "B", 2: "C"}
+	// need to handle more column,,,
+	m := map[int]string{0: "A", 1: "B", 2: "C", 3: "D", 4: "E", 5: "F"}
 	line_index_str := strconv.Itoa(line_index)
 	for i := 0; i < len(row); i++ {
 		area := m[i] + line_index_str
